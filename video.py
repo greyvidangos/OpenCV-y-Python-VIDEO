405b587fd33538925b2b7eed411454677e05e5f3
grey <31018622+greyvidangos@users.noreply.github.com>
2019-03-02 11:50:51 +0300

    Update video.py

--- video.py
+++ video.py
@@ -1,2 +1,27 @@
 import cv2
 import numpy as np
+import pafy # pip3 install youtube-dl
+
+url = "https://www.youtube.com/watch?v=bjzkmHRLTaI"
+video = pafy.new(url)
+best = video.getbest(preftype="mp4")
+
+#captura = cv2.VideoCapture('drone.mp4')                 # Video File
+#captura = cv2.VideoCapture(0)                           # Webcam 1,2,3
+#captura = cv2.VideoCapture('http://192.168.1.68:8081/') # IPCamara
+
+captura = cv2.VideoCapture() #Youtube
+captura.open(best.url)
+
+while True:
+    ret, frame = captura.read()
+
+
+    
+    cv2.imshow('Salida',frame)
+    k = cv2.waitKey(24)&0xFF
+    if k == 27:
+        break
+    
+captura.release()
+cv2.destroyAllWindows()
